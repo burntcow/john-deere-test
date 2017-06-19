5ceefa26157bb09e6d07216500b82b70b52af417
Josh Manders <josh@joshmanders.com>
2017-06-19 15:52:12 -0500

    flesh out tests

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import App from '../src/app.jsx';
 import Filter from '../src/filter.jsx';
 import { pizzas } from '../pizza.json';
 
 describe('App View', () => {
-    it('should resolve pizzas and return Filter', () => {
-      const wrapper = mount(<App />);
-      wrapper.setState({ pizzas });
+  it('should show a loading screen when no pizzas loaded', () => {
+    const wrapper = shallow(<App />);
 
-      expect(wrapper.state('pizzas')).toEqual(pizzas);
-      expect(wrapper.contains(<Filter items={pizzas} />)).toEqual(true);
-    });
+    expect(wrapper.text()).toEqual('Loading...');
+  });
+
+  it('should resolve pizzas and return Filter', () => {
+    const wrapper = mount(<App />);
+    wrapper.setState({ pizzas });
+
+    expect(wrapper.state('pizzas')).toEqual(pizzas);
+    expect(wrapper.contains(<Filter items={pizzas} />)).toEqual(true);
+  });
 });
--- test/filter.spec.js
+++ test/filter.spec.js
@@ -5,19 +5,74 @@ import { pizzas } from '../pizza.json';
 
 describe('Filter View', () => {
     it('should have default state', () => {
-      const wrapper = mount(<Filter items={pizzas} />);
+      const wrapper = mount(<Filter />);
 
       expect(wrapper.state('filter')).toEqual('');
       expect(wrapper.state('sort')).toEqual(false);
     });
 
-    it('', () => {
+    it('should have a list of pizzas shown', () => {
       const wrapper = mount(<Filter items={pizzas} />);
 
-      const filter = wrapper.find('input[type="text"]');
+      expect(wrapper.props().items).toEqual(pizzas);
+    });
+
+    it('should filter pizzas from input', () => {
+      const wrapper = mount(<Filter items={pizzas} />);
+
+      const filter = wrapper.find('[type="text"]');
 
       filter.simulate('change', { target: { value: 'Pepp' } });
 
-      console.log(wrapper.html());
+      const items = wrapper.find('li');
+
+      expect(items.length).toEqual(2);
+      expect(items.at(0).text()).toEqual('Pepperoni');
+      expect(items.at(1).text()).toEqual('Sausage and Pepperoni');
+    });
+
+    it('should filter in a case insensitive way', () => {
+      const wrapper = mount(<Filter items={pizzas} />);
+
+      const filter = wrapper.find('[type="text"]');
+
+      filter.simulate('change', { target: { value: 'pepp' } });
+
+      const items = wrapper.find('li');
+
+      expect(items.length).toEqual(2);
+      expect(items.at(0).text()).toEqual('Pepperoni');
+      expect(items.at(1).text()).toEqual('Sausage and Pepperoni');
+    });
+
+    it('should sort and list pizzas in reverse alphabetic order', () => {
+      const wrapper = mount(<Filter items={pizzas} />);
+
+      const sort = wrapper.find('[type="button"]');
+
+      sort.simulate('click');
+
+      const items = wrapper.find('li');
+
+      expect(items.length).toEqual(pizzas.length);
+      expect(items.at(0).text()).toEqual('vegetable');
+    });
+
+    it('should also reverse alphabetic sort filtered pizzas', () => {
+      const wrapper = mount(<Filter items={pizzas} />);
+
+      const filter = wrapper.find('[type="text"]');
+
+      filter.simulate('change', { target: { value: 'pepp' } });
+
+      const sort = wrapper.find('[type="button"]');
+
+      sort.simulate('click');
+
+      const items = wrapper.find('li');
+
+      expect(items.length).toEqual(2);
+      expect(items.at(0).text()).toEqual('Sausage and Pepperoni');
+      expect(items.at(1).text()).toEqual('Pepperoni');
     });
 });
